960d1ff2c14238423e98b5599f8a6fefe8a57e27
Khushal Patil <58685911+khushalpatil093@users.noreply.github.com>
2023-07-16 11:41:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 //complete this code
-class Person {}
+class Person {
 
-class Student extends Person {}
+	constructor(name) {
+		this._name = name;
+		this._age = age;
+	}
+	
+	get name() {
+		return this._name;
+	}
+	set age(newAge) {
+		this._age = newAge;
+	}
+	
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+	study() {
+		console.log(this._name+" is studying");
+	}
+}
+
+class Teacher extends Person {
+	teach() {
+		console.log(this._name+" is teaching");
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
